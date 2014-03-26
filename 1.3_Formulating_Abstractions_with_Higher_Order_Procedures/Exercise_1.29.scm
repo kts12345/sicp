6d842b10c3e5b1a2719b5d2d59d430329f9455d5
rain123123 <rainsnow1234567@gmail.com>
2014-03-26 19:16:21 +0900

    scm exercise 1.29

---
+++ 1.3_Formulating_Abstractions_with_Higher_Order_Procedures/Exercise_1.29.scm
@@ -0,0 +1,28 @@
+﻿(define (sum term a next b)
+  (if (> a b)
+	0
+	(+ (term a)
+	   (sum term (next a) next b))))
+
+
+(define (cube x) (* x x x))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+	 dx))
+
+
+(define (simpsons_rule f a b n)
+  (define (h) (/ (- b a) n))
+  (define (add-dx x) (+ x 1))
+  (define (y_k k) (f (a + (* k h))))
+  (define (term k)
+	(cond ((= k 0) (y_k k))
+		  ((= k n) (y_k k))
+		  (= (even? k) (* 4 (y_k k)))
+		  (else (* 2 (y_k k)))))
+  (* (sum term 0 add-dx n)
+	 (/ h 3)))
+
+
