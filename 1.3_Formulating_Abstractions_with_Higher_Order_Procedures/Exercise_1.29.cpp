2d4fcfc694b0a188abb4cf2f637dd39803f84e56
kts12345 <taeksoon@gmail.com>
2014-03-26 18:52:32 +0900

    ..

--- 1.3_Formulating_Abstractions_with_Higher_Order_Procedures/Exercise_1.29.cpp
+++ 1.3_Formulating_Abstractions_with_Higher_Order_Procedures/Exercise_1.29.cpp
@@ -14,31 +14,6 @@ shown above.
 
 
 
-/*
-cube x = x * x * x
-
-simpsons_rule f a b n =
-let
-h = (b - a) / n
-in
-(h / 3.0) * (sum $ g f h 0 n)
-where
-g f h k n | k == 0 = f(a + k * h) : g f h(k + 1) n
-| k == n = f(a + k * h) : []
-| mod k 2 == 0 = 2 * f(a + k * h) : g f h(k + 1) n
-| otherwise = 4 * f(a + k * h) : g f h(k + 1) n
-
-
-main = do
-print $ simpsons_rule cube 0 1 100
-print $ simpsons_rule cube 0 1 1000
-*/
-
-//-------------------------------------------------------------
-//  (define(cube x) (*x x x))
-//
-auto cube = _1 * _1 * _1;
-
 int main()
 {
   auto print = [](auto v) { std::cout << v << std::endl; };
