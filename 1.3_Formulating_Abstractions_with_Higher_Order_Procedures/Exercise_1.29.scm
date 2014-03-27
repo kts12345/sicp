265fc0318397a5319281184cfd9fd07d5ecc4958
rain123123 <rainsnow1234567@gmail.com>
2014-03-27 10:27:40 +0900

    컴파일 됨

--- 1.3_Formulating_Abstractions_with_Higher_Order_Procedures/Exercise_1.29.scm
+++ 1.3_Formulating_Abstractions_with_Higher_Order_Procedures/Exercise_1.29.scm
@@ -1,8 +1,7 @@
 ﻿(define (sum term a next b)
   (if (> a b)
-	0
-	(+ (term a)
-	   (sum term (next a) next b))))
+       0 (+ (term a)
+          (sum term (next a) next b))))
 
 
 (define (cube x) (* x x x))
@@ -10,19 +9,26 @@
 (define (integral f a b dx)
   (define (add-dx x) (+ x dx))
   (* (sum f (+ a (/ dx 2.0)) add-dx b)
-	 dx))
+     dx))
 
 
 (define (simpsons_rule f a b n)
   (define (h) (/ (- b a) n))
   (define (add-dx x) (+ x 1))
-  (define (y_k k) (f (a + (* k h))))
+  (define (y_k k) (f (+ a (* k (h)))))
   (define (term k)
-	(cond ((= k 0) (y_k k))
-		  ((= k n) (y_k k))
-		  (= (even? k) (* 4 (y_k k)))
-		  (else (* 2 (y_k k)))))
-  (* (sum term 0 add-dx n)
-	 (/ h 3)))
+    (cond ((= k 0) (y_k k))
+          ((= k n) (y_k k))
+          (= (even? k) (* 4 (y_k k)))
+          (else (* 2 (y_k k)))))
+    (* (sum term 0 add-dx n)
+       (/ (h) 3)))
 
+(simpsons_rule cube 0 1 100)
+(simpsons_rule cube 0 1 1000)
+
+
+----------------------------------
+9901/30000
+999001/3000000
 
