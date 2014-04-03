0dfc540a11b28068c84148efb0e5d9abb07613d7
rain123123 <rainsnow1234567@gmail.com>
2014-04-03 20:55:02 +0900

    scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34scm 1.34

---
+++ 1.3_Formulating_Abstractions_with_Higher_Order_Procedures/Exercise_1.34.scm
@@ -0,0 +1,19 @@
+﻿(define (f g)
+  (g 2))
+
+(define (square x)
+  (* x x))
+
+(f square)
+
+(f (lambda (z) (* z (+ z 1))))
+
+(f f)
+
+ ; application: not a procedure;
+ ; expected a procedure that can be applied to arguments
+ ;    given: 2
+ ;    arguments...:
+ ;      2
+
+
